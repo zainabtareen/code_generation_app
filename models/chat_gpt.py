245502461a7c59a26160a10f749ef392a889eb9a
Zainab <14beeztareen@seecs.edu.pk>
2024-03-26 07:17:45 +0500

    loading api key from env

--- models/chat_gpt.py
+++ models/chat_gpt.py
@@ -1,6 +1,13 @@
+import os
+
+from dotenv import load_dotenv
 from openai import OpenAI
 
-client = OpenAI()
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.environ['OPENAI_API_KEY']
+)
 
 
 def invoke_chat_gpt(messages):
